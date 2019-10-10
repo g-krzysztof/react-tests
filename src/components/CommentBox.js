5196b71d444fa26249c725dbbfe1dc75fc7da290
Krzysztof Gryc <hello@krzysztofg.pl>
2019-10-10 13:04:03 +0200

    add basic logic to CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
 class CommentBox extends React.Component {
-    state = {}
+    state = {
+        comment: ''
+    };
+
+    handleChange = e => {
+        this.setState({
+            comment: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        // TODO - Call an action creator
+        // And save the comment
+
+        this.setState({ comment: '' });
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h4>Add a comment</h4>
-                <textarea></textarea>
+                <textarea
+                    value={this.state.comment}
+                    onChange={this.handleChange}
+                >
+                </textarea>
                 <div>
                     <button>Submit Comment</button>
                 </div>
